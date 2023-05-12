5ca2360a86d7649277687889357c61edc2bb8405
Shadvonic <moxeymarcus28@gmail.com>
2023-05-12 16:24:34 -0400

    state property

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import './App.css';
 import Employee  from './components/employee'; // import to used employee components 
+import { useState } from 'react'; 
 
 
 function App() {
+  const [role, setRole] = useState('dev');
 
   const showEmployees = true; 
   return (
     <div className="App">
      
        
-      { showEmployees ? 
-  
-
+      { showEmployees ? (
       <> 
+      <input type='text' onChange={(e) => { 
+        console.log(e.target.value);
+        setRole(e.target.value)
+        }} />
        <Employee name="Marc" role="intern" />
-       <Employee name="Elena"/>
+       <Employee name="Elena" role={role}/>
        <Employee  name ="Caleb"/>
       </>
       
-       : 
+    ): (
        <p>You cannot see the employees</p>
 
-    }
+    )}
+
     </div>
   );
 }
--- src/components/employee.js
+++ src/components/employee.js
@@ -1,16 +1,11 @@
 // creating a component as a function so it can be reused
 function Employee(props) {
+ 
     return (
         <> 
          <h3>Employee {props.name} </h3>
          <p>{props.role ? props.role : "No role"}</p> 
-         {props.role ? ( 
          
-         <p className="role">{props.role}</p>
-         ) : ( 
-
-         <p className="no role">No role</p> 
-         )}  {/* use only if you want to display something different */ }
         </>
        
     )
