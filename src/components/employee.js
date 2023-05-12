79f2ffd6459345026aa574c86f12aa8263e4ffc6
Shadvonic <moxeymarcus28@gmail.com>
2023-05-12 16:14:35 -0400

    props and passing data

--- src/components/employee.js
+++ src/components/employee.js
@@ -1,6 +1,19 @@
 // creating a component as a function so it can be reused
-function Employee() {
-    return <h3>Here is an employee</h3>; 
+function Employee(props) {
+    return (
+        <> 
+         <h3>Employee {props.name} </h3>
+         <p>{props.role ? props.role : "No role"}</p> 
+         {props.role ? ( 
+         
+         <p class="role">{props.role}</p>
+         ) : ( 
+
+         <p class="no role">No role</p> 
+         )}  {/* use only if you want to display something different */ }
+        </>
+       
+    )
 }
 
 export default Employee;;
